43233462db50b08c963d8799d2610f0458d07a18
Samael Musgrove <developersamaelmusgrove@gmail.com>
2023-01-24 15:12:46 -0600

    /

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
-      <Nav />
-      <Header />
     </div>
   );
 }
