373d1ad259193bccee35d3390a44a01ea8722671
Samael Musgrove <developersamaelmusgrove@gmail.com>
2023-01-24 04:42:10 -0600

    Updated Nav.jsx
    Added basic features for Navbar using Tailwind using a Pepsi Logo for testing and size comparison

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div class="relative bg-white mx-auto max-w-7xl px-6 flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10 lg:w-0 lg:flex-1">
+      <a href="/">
+        <span className="sr-only">Your Company</span>
+        <img class="h-8 w-auto sm:h-10" src="https://1000logos.net/wp-content/uploads/2017/05/Pepsi-Logo.png" alt=""></img>
+      </a>
+    </div>
   )
 }
 
