e373410d0a6acd318f223c6eae3ec0679cb7e8ad
Samael Musgrove <developersamaelmusgrove@gmail.com>
2023-01-26 15:33:35 -0600

    Transparent Nav

--- portfolio/src/Pages/Main.jsx
+++ portfolio/src/Pages/Main.jsx
@@ -5,9 +5,6 @@ const Main = () => {
   return (
     <div id="Home">
       <img id="Homeimg"className='w-full h-screen object-cover' src={DevSam} alt="Hero img" />
-      <div className='flex justify-center items-center'>
-        <h1 className='text-white text-6xl'>Welcome</h1>
-      </div>
     </div>
   )
 }
--- portfolio/src/components/Button.jsx
+++ portfolio/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const button = (props) => {
   return (
-    <button className='bg-sky-500 text-white font-sans py-2 px-6 rounded md:ml-8 hover:bg-sky-800 duration-500 font-bold'>
+    <button className='bg-sky-500 text-white font-sans py-2 px-6 rounded md:ml-10 hover:bg-sky-800 duration-500 font-bold'>
         {props.children}
     </button>
   )
