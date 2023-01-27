6006d8a0ec4c99e41e093865a7ffedd3f474e51f
Samael Musgrove <developersamaelmusgrove@gmail.com>
2023-01-27 12:36:11 -0600

    added about me and working on imgs size and text size

--- portfolio/src/Pages/Main.jsx
+++ portfolio/src/Pages/Main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import DevSam from '../Assets/DevSam.gif'
-import Division from '../Assets/Division.png'
+import Me from'../Assets/Me.png'
 
 const Main = () => {
   return (
     <>
         <div id="Home">
-        <img className='w-full h-screen object-cover' src={DevSam} alt="Hero img" />
+            <img className='w-full h-screen object-cover' src={DevSam} alt="Hero img" />
+        </div>
+        <div id="About Me" className='w-full h-screen bg-black'>
+            <h1 className='text-6xl font-mono text-white font-bold flex ml-5 my-20'>
+                About Me!
+            </h1>
+            <img class='float-right rounded-lg' src={Me} alt="Mobile" />
+            <p className='text-xl font-mono text-white py-14 ml-2 px-1 border-red-50'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut enim blandit volutpat maecenas. Vitae purus faucibus ornare suspendisse sed nisi lacus. Porta non pulvinar neque laoreet suspendisse interdum consectetur. Ultricies mi quis hendrerit dolor. Auctor urna nunc id cursus. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Ac turpis egestas sed tempus urna. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Dolor sed viverra ipsum nunc aliquet bibendum. Ornare aenean </p>
+          
         </div>
     </>
   )
