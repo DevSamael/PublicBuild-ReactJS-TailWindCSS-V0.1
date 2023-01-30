5171e75fe5ce7ae6ff94905cffb733c44b4d6bcf
Samael Musgrove <developersamaelmusgrove@gmail.com>
2023-01-29 21:23:03 -0600

    removed unneccesarys

--- portfolio/src/Pages/Main.jsx
+++ portfolio/src/Pages/Main.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import '../Pages/Main.css'
-import sale from '../Assets/sale.png'
 
 const Main = () => {
   return (
     <>
       <div id="Home">
         <div className='w-full h-screen bg-videobg bg-cover bg-center bg-fixed flex flex-col justify-center items-center'>
-          <img className='w-full h-auto' src={sale} alt="" />
           
         </div>
         <div id='About' className='w-full h-screen bg-black'>
-              
         </div>
       </div>
     </>
