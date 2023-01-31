d805908be0a8d7d1c630ee757c8325791c16f6cd
Samael Musgrove <developersamaelmusgrove@gmail.com>
2023-01-31 16:17:45 -0600

    /

--- portfolio/src/Pages/Main.jsx
+++ portfolio/src/Pages/Main.jsx
@@ -5,7 +5,9 @@ const Main = () => {
     <>
       <div id="Home">
         <div className='w-full h-screen bg-videobg bg-cover bg-center bg-fixed flex flex-col justify-center items-center'>
-          
+          <h1 id='headerh1' className='font-bold font-sans text-5xl text-white'>
+            Big Dick Sam
+          </h1>
         </div>
         <div id='About' className='w-full h-screen bg-black'>
         </div>
